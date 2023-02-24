9a0eac63fb1b2d1b424cadebb151694128d9d55f
SaeBom Park <bombom@bombom-MacBookAir.local>
2023-02-25 05:29:56 +0900

    중간저장

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  textColor: "#2f3640",
+  bgColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+};
+
+// export const lightTheme: DefaultTheme = {
+//   textColor: "black",
+//   bgColor: "white",
+// };
+
+// export const darkTheme: DefaultTheme = {
+//   textColor: "white",
+//   bgColor: "black",
+// };
